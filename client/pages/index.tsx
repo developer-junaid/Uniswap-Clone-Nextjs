edf28b63d2da091aab512a8e421743d28655e4b0
Developer Junaid <qjunaid623@gmail.com>
2022-08-14 12:01:12 +0500

    main completed

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 
 // Components
 import Header from "../components/Header";
+import Main from "../components/Main";
 
 const style = {
   wrapper: `h-screen max-h-screen min-h-screen w-screen bg-[#2d242f] text-white select-none flex flex-col justify-between`,
@@ -11,7 +12,7 @@ const Home: NextPage = () => {
   return (
     <div className={style.wrapper}>
       <Header />
-      <h2>Main</h2>
+      <Main />
       <h2>TransactionHistiory</h2>
     </div>
   );
